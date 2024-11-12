66bb24833ff77aeaea45097efe67a150102405e1
Leandro Soto <124328946+leandruko@users.noreply.github.com>
2024-11-12 17:22:19 -0300

    update

--- card-lea/tailwind.config.mjs
+++ card-lea/tailwind.config.mjs
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
+      colors:{
+        'negro':'#181C14',
+        'verde':'#697565',
+        'claro':'#ECDFCC',
+        'gris':'#3C3D37'
+      },
+    },
+  },
+  screens: {
+    'sm': '576px',
+    // => @media (min-width: 576px) { ... }
+
+    'md': '960px',
+    // => @media (min-width: 960px) { ... }
+
+    'lg': '1440px',
+    // => @media (min-width: 1440px) { ... }
+  },
+  plugins: [],
 }
